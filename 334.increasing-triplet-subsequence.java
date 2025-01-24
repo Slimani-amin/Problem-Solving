5712cd6572980b5507624ef497924bc95bd9f413
Slimani-amin <amineslimani1610@gmail.com>
2025-01-24 06:52:33 +0100

    add problem 334

---
+++ 334.increasing-triplet-subsequence.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=334 lang=java
+ *
+ * [334] Increasing Triplet Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= a) {
+                a = num;
+            } else if (num <= b) {
+                b = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// @lc code=end
+
