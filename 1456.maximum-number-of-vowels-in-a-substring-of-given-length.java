b4291cdd7b4668e200c6d4f5af65999c85087da5
Slimani-amin <amineslimani1610@gmail.com>
2025-01-28 16:11:09 +0100

    add problem 1456

---
+++ 1456.maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=1456 lang=java
+ *
+ * [1456] Maximum Number of Vowels in a Substring of Given Length
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isVowel(char c) {
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    }
+    
+    public int maxVowels(String s, int k) {
+        int n = s.length();
+        int maxV = 0;
+        
+        // Compter les voyelles dans la première fenêtre de taille k
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) {
+                maxV++;
+            }
+        }
+        
+        int currentCount = maxV;
+        
+        // Parcourir les fenêtres de taille k
+        for (int i = 1; i <= n - k; i++) {
+            if (maxV == k) {
+                return k; // Si on atteint le maximum possible, on retourne directement
+            }
+            
+            // Ajuster le compteur en fonction de la fenêtre glissante
+            if (!isVowel(s.charAt(i - 1)) && isVowel(s.charAt(i + k - 1))) {
+                currentCount++;
+            } else if (isVowel(s.charAt(i - 1)) && !isVowel(s.charAt(i + k - 1))) {
+                currentCount--;
+            }
+            
+            // Mettre à jour le maximum
+            if (currentCount > maxV) {
+                maxV = currentCount;
+            }
+        }
+        
+        return maxV;
+    }
+}
+
+// @lc code=end
+
